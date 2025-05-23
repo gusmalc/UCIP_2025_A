8f03035acb064497c86278f85968ae831219e3fc
Gustavo Malcotti <gustavo@malcotti.com>
2025-05-23 20:59:16 -0300

    1

---
+++ clase_05/teoria_js/js/scripts.js
@@ -0,0 +1,37 @@
+let nombre = "Maria"; // string
+let edad = 25; // number
+let estudia = true; // boolean
+
+let a = 10;
+let b = 20;
+let e = 20;
+
+let c = a / b;
+
+const xx = 35;
+
+
+//console.log(c);
+
+//alert(nombre);
+
+
+// let dias = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
+
+// let persona = {
+//     nombre: "Brenda",
+//     edad: 23,
+//     estdia: true
+// };
+
+// console.log(persona.edad);
+
+function suma(a,b){
+    let c = a + b;
+    alert(c);
+}
+
+
+suma(30,50);
+
+suma(23,30);
